2d3853b4b374ddc6ab6e7a0a5fd47c777bddc4fe
Rifaat-Ahammed <rifatahammed2020@gmail.com>
2023-05-09 13:55:40 +0300

    API updated

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('owners', \App\Http\Controllers\OwnerAPIContoller::class);
+Route::resource('cars', \App\Http\Controllers\CarAPIContoller::class);
